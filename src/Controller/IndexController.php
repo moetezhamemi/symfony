b257d0fca70c7192c7cb0363c1c12a1f7d2e383d
moetezhamemi <moetezhamemi18@gmail.com>
2025-02-20 09:35:36 +0100

    creation de controller (tp2)

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/{name}', name: 'home')]
+    public function home(string $name): Response
+    {
+        return $this->render('index.html.twig', ['name' => $name]);
+    }
+}
